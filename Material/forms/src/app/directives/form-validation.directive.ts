e7e0d610261db8879e4eaa23d5a3d1c4bc5192c5
Rodrigo Muniz <rodrigotmuniz@gmail.com>
2018-08-09 00:22:27 -0300

    novo

--- Material/forms/src/app/directives/form-validation.directive.ts
+++ Material/forms/src/app/directives/form-validation.directive.ts
@@ -4,6 +4,9 @@ import { Directive, HostBinding, Input, Renderer, ElementRef, Renderer2 } from '
   selector: '[formValidation]'
 })
 export class FormValidationDirective {
+
+  messageElement;
+  iconElement;
   
   @Input() touched = false;
   @HostBinding('class') classes = 'form-group col-sm-12';
@@ -11,16 +14,13 @@ export class FormValidationDirective {
   @Input() set formValidation(condition) {
     if(condition && this.touched) {
       this.classes = 'form-group col-sm-12 has-error has-feedback';
-      
-      const div = this.renderer.createElement('div');
-      const text = this.renderer.createText('Campo obrigatóriosss');
-      
-      this.renderer.appendChild(div, text);
-      this.renderer.appendChild(this.el.nativeElement, div);
+      this.renderer.setStyle(this.messageElement, 'display', 'block');
+      this.renderer.setStyle(this.iconElement, 'display', 'block')
     }
     else {
       this.classes = this.touched ? 'form-group col-sm-12 has-success has-feedback' : 'form-group col-sm-12';
-      
+      this.messageElement ? this.renderer.setStyle(this.messageElement, 'display', 'none') : null;
+      this.iconElement ? this.renderer.setStyle(this.iconElement, 'display', 'none') : null;
     }
   };
   
@@ -28,16 +28,32 @@ export class FormValidationDirective {
     private el: ElementRef,
     private renderer: Renderer2
   ) { 
-    // this._renderer.setElementStyle(this._elementRef.nativeElement, 'background-color', 'yellow');
-    // console.log(this._elementRef.nativeElement);
-    
-    
-    
-    
   }
   
   ngOnInit() {
     
+  }
+  ngAfterContentInit() {
+    this.initializeIconElement();
+    this.initializeMessageElement();
+  }
+
+  initializeMessageElement() {
+    this.messageElement = this.renderer.createElement('messageElement');
+    this.renderer.addClass(this.messageElement, 'help-block');
+    const text = this.renderer.createText('Campo obrigatóriosss');
+    this.renderer.appendChild(this.messageElement, text);
+    this.renderer.appendChild(this.el.nativeElement, this.messageElement);
+    this.renderer.setStyle(this.messageElement, 'display', 'none')
+  }
+
+  initializeIconElement() {
+    this.iconElement = this.renderer.createElement('messageElement');
+    this.renderer.addClass(this.iconElement, 'glyphicon');
+    this.renderer.addClass(this.iconElement, 'glyphicon-remove');
+    this.renderer.addClass(this.iconElement, 'form-control-feedback');
+    this.renderer.appendChild(this.el.nativeElement, this.iconElement);
+    this.renderer.setStyle(this.iconElement, 'display', 'none')
   }
   
 }
